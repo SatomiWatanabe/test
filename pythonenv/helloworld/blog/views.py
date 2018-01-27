af61e6f1a835249330f053379fef87c23114aa47
SatomiWatanabe <supreme-bliss@outlook.com>
2018-01-27 19:50:35 +0900

    third

--- pythonenv/helloworld/blog/views.py
+++ pythonenv/helloworld/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def article(request):
+	queryset_list = Post.objects.all()
+
+	context = {
+		"object_list": queryset_list,
+		}
+	return render(request, "articles.html", context)
