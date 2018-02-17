50bdb0000ec8e1923bc10e35688d4093f3eb7d71
SatomiWatanabe <supreme-bliss@outlook.com>
2018-02-17 18:34:24 +0900

    design revised

--- pythonenv/helloworld/blog/views.py
+++ pythonenv/helloworld/blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .models import *
 from .forms import *
+from django.db.models import Q
 
 # Create your views here.
 def article(request):
-	queryset_list = Post.objects.all()
+	queryset_list = Post.objects.all().order_by('-time')
+	query = request.GET.get("q")
+		
+	if query:
+		queryset_list = Post.objects.all().filter(Q(message__contains=query)).order_by('-time')
+
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
